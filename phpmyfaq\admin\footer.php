bccd7e4ba89414c83754e7ef755397f3c467cdef
Toruma <KCeylan@noctron-de>
2014-11-27 13:25:13 +0100

    phpmyfaq\admin\footer.php
    Admin-Section\Footer;
    "Follow us on Twitter" + "phpmyfaq documentation"

---
+++ phpmyfaq\admin\footer.php
@@ -0,0 +1,7 @@
+// Entfernen von "follow us on twitter" + "phpmyfaq documentation"
+// phpmyfaq\admin\footer.php
+
+// Original
+
+47| <a href="http://www.phpmyfaq.de/documentation.php" target="_blank">phpMyFAQ documentation</a>
+48| Follow us on <a href="http://twitter.com/phpMyFAQ">Twitter</a> |
